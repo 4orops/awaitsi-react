b0eb2a578b7f6b6312a1ca9035a99bafe3d54ac3
4orops <wabile4@gmail.com>
2026-02-24 10:11:06 +0200

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,13 @@ import { RouterProvider } from 'react-router-dom'
 import './App.css'
 import router from './routes'
 
-createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root')
+
+if (!rootElement) {
+  throw new Error('Failed to find the root element')
+}
+
+createRoot(rootElement).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>,
